203d5ce3ae87fcee07b855435096a61dafa1e329
Tien <tienlongz@gmail.com>
2014-09-20 23:32:04 +0800

    Modified functions of the observable list to return boolean if the operation is successful These functions includes : add, delete, edit
    Passed unit testing for adding into null list

--- WeDo/src/dataStorage/ObservableList.java
+++ WeDo/src/dataStorage/ObservableList.java
@@ -1,114 +1,151 @@
 package dataStorage;
+
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
-
 /**
- * This class consist the observable list which update it's observers whenever a modification occurs
- * The possible modification includes : 
- * <li> add 
- * <li> delete
- * <li> edit
- * <li> replace list
- * <p><br>
+ * This class consist the observable list which update it's observers whenever a
+ * modification occurs The possible modification includes : <li>add <li>delete
+ * <li>edit <li>replace list
+ * <p>
+ * <br>
  * 
- * @author Kuan Tien Long  
+ * @author Kuan Tien Long
  * 
- * @param <T> the list type 
+ * @param <T>
+ *            the list type
  */
-public class ObservableList<T> extends Observable 
-{
-    ArrayList<T> observedList = new ArrayList<T>();
-    public ObservableList(ArrayList<T> list)
-    {
+public class ObservableList<T> extends Observable {
+    private ArrayList<T> observedList;
+
+    public ObservableList(ArrayList<T> list) {
         observedList = list;
     }
-    
+
     /**
-     * This function add the argument to the list and update the observer(s) with the argument
-     * @param argument to be added to the list
+     * @return whether if the list is empty or null
      */
-    public void add(T argument)
-    {
-       observedList.add(argument);
-       setChanged();
-       notifyObservers(argument);
+    private boolean isEmpty() {
+        return observedList == null || observedList.isEmpty();
     }
-    
+
     /**
-     * This function delete the specified index of the list and update the observer(s) about with the deleted argument 
-     * @param index to be deleted from the list
+     * This function add the argument to the list and update the observer(s)
+     * with the argument
+     * 
+     * @param argument
+     *            to be added to the list
+     * @return boolean if the operation is successful
      */
-    public void delete(int index)
-    {
+    public boolean add(T argument) {
+        if (isEmpty()) {
+            return false;
+        }
+        observedList.add(argument);
+        setChanged();
+        notifyObservers(argument);
+        return true;
+    }
+
+    /**
+     * This function delete the specified index of the list and update the
+     * observer(s) about with the deleted argument
+     * 
+     * @param index
+     *            to be deleted from the list
+     * @return boolean if the operation is successful
+     */
+    public boolean delete(int index) {
+        if (isEmpty()) {
+            return false;
+        }
         T deleted = observedList.remove(index);
         setChanged();
         notifyObservers(deleted);
+        return true;
     }
-    
+
     /**
-     * This function delete the specified argument of the list and update the observer(s) about with the deleted argument
-     * @param argument to be deleted from the list
+     * This function delete the specified argument of the list and update the
+     * observer(s) about with the deleted argument
+     * 
+     * @param argument
+     *            to be deleted from the list
+     * @return boolean if the operation is successful
      */
-    public void delete(T argument)
-    {
-       observedList.remove(argument);
-       setChanged();
-       notifyObservers(argument);
-    }
-    
-    /**
-     * This function edit the specified index of the list with the argument.
-     * <br> In addition, it also updates the observer(s) about with the edited argument
-     * @param index that is to be selected for editing
-     * @param argument that will be replacing the initial information
-     */
-    public void edit(int index, T argument)
-    {
-        observedList.set(index, argument);
+    public boolean delete(T argument) {
+        if (isEmpty()) {
+            return false;
+        }
+        observedList.remove(argument);
         setChanged();
         notifyObservers(argument);
+        return true;
     }
-    
+
     /**
-     * This function replaces the entire list.
-     * <br> In addition, it also updates the observer with the new list
-     * @param newList which will be used to replace the initial list.
+     * This function edit the specified index of the list with the argument. <br>
+     * In addition, it also updates the observer(s) about with the edited
+     * argument
+     * 
+     * @param index
+     *            that is to be selected for editing
+     * @param argument
+     *            that will be replacing the initial information
+     * @return boolean if the operation is successful
      */
-    public void replaceList(ArrayList<T> newList)
-    {
+    public boolean edit(int index, T argument) {
+        try {
+            observedList.set(index, argument);
+        } catch (IndexOutOfBoundsException exception) {
+            return false;
+        }
+        setChanged();
+        notifyObservers(argument);
+        return true;
+    }
+
+    /**
+     * This function replaces the entire list. <br>
+     * In addition, it also updates the observer with the new list
+     * 
+     * @param newList
+     *            which will be used to replace the initial list.
+     */
+    public void replaceList(ArrayList<T> newList) {
         observedList = newList;
         setChanged();
         notifyObservers(newList);
     }
-    
+
     /**
-     * This function retrieve the argument from the specified index of the list 
-     * @param index of the argument to return
+     * This function retrieve the argument from the specified index of the list
+     * 
+     * @param index
+     *            of the argument to return
      * @return the argument at the specified index
      */
-    public T get(int index)
-    {
+    public T get(int index) {
         return observedList.get(index);
     }
-    
+
     /**
      * @return the stored list
      */
-    public ArrayList<T> getList()
-    {
+    public ArrayList<T> getList() {
         return observedList;
-        
+
     }
-    
+
     /**
-     * This function add observer to the list. 
-     * <br>The observer will be updated when modification to the list is made
-     * @param observer that will be added to observed the modification in the list
+     * This function add observer to the list. <br>
+     * The observer will be updated when modification to the list is made
+     * 
+     * @param observer
+     *            that will be added to observed the modification in the list
      */
-    public void addObserverList(Observer observer)
-    {
+    public void addObserverList(Observer observer) {
         this.addObserver(observer);
     }
 }
