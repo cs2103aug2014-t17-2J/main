e2c7803190bbde21e726cba0808915437bbf82f1
maryam <maryamisme@gmail.com>
2014-09-20 20:03:46 +0800

    updated UserInterface

---
+++ WeDo/src/UserInterface.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+
+public class UserInterface {
+	
+	private final static ArrayList<String> list = new ArrayList<String>();
+
+	public UserInterface() {
+		
+		DataHandler dataHand = new DataHandler();
+		CommandHandler commHand = new CommandHandler();
+		
+	}
+
+	public static void main(String[] args) {
+
+		 list = dataHand.getList();
+	}
+
+}
