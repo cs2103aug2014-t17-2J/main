36151c9714798a5b1eabea00227b8ec8d1109a7a
maryam <maryamisme@gmail.com>
2014-09-20 19:24:05 +0800

    created the main class

---
+++ WeDo/src/CommandHandler.java
@@ -0,0 +1,13 @@
+
+public class CommandHandler {
+
+	public CommandHandler() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ WeDo/src/DataHandler.java
@@ -0,0 +1,13 @@
+
+public class DataHandler {
+
+	public DataHandler() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
