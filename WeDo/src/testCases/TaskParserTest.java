bb93ecb0b96db4e08deef5c1d71c96086810e648
Tien <tienlongz@gmail.com>
2014-09-24 23:29:15 +0800

    Added algorithm for setting of task priority

--- WeDo/src/testCases/TaskParserTest.java
+++ WeDo/src/testCases/TaskParserTest.java
@@ -16,6 +16,7 @@ import logic.TaskParser;
 import org.junit.Test;
 
 import dataStorage.DataHandler;
+import definedEnumeration.Priority;
 
 /**
  * @author Kuan Tien Long
@@ -34,16 +35,18 @@ public class TaskParserTest {
 
         expectedTask.setStartDate(LocalDate.of(2014, 9, 12));
         expectedTask.setDescription("momo");
+        expectedTask.setPriority(Priority.PRIORITY_HIGH);
         assertEquals(expectedTask,
                 taskParser.buildTask("-add momo -date 12 sept -priority high"));
 
         expectedTask.setStartDate(LocalDate.of(2014, 9, 12));
         expectedTask.setEndDate(LocalDate.of(2014, 9, 18));
         expectedTask.setDescription("buy for me something");
+        expectedTask.setPriority(Priority.PRIORITY_LOW);
         assertEquals(
                 expectedTask,
                 taskParser
-                        .buildTask("-add buy for me something -date 12 sept to 18 sept -priority high"));
+                        .buildTask("-add buy for me something -date 12 sept to 18 sept -priority low"));
 
         expectedTask.setStartDate(LocalDate.of(2014, 9, 18));
         expectedTask.setStartTime(LocalTime.of(14, 0));
