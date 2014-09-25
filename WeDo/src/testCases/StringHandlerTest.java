cceb67ffb789b1bd533bc1e23c2920f812207edb
Tien <tienlongz@gmail.com>
2014-09-25 14:58:36 +0800

    Refactored test cases for StringHandler

--- WeDo/src/testCases/StringHandlerTest.java
+++ WeDo/src/testCases/StringHandlerTest.java
@@ -10,20 +10,28 @@ public class StringHandlerTest {
 
     @Test
     public void test() {
+        getFirstWordTest();
+        
+        removeFirstMatchedTest();
+        
+            
+    }
+
+    private void getFirstWordTest() {
         assertEquals("add", StringHandler.getFirstWord("add Pizza"));
         assertEquals(null, StringHandler.getFirstWord(""));
         assertEquals(null, StringHandler.getFirstWord("       "));
         assertEquals(null, StringHandler.getFirstWord(null));
         assertEquals("greedIsGood", StringHandler.getFirstWord("greedIsGood"));
         assertEquals("Pig", StringHandler.getFirstWord("Pig is flying !!!"));
-        
+    }
+
+    private void removeFirstMatchedTest() {
         assertEquals("Pizza", StringHandler.removeFirstMatched("add Pizza", "add"));
         assertEquals("pig eat me", StringHandler.removeFirstMatched("pig eat me","add"));
         assertEquals("pig eat me", StringHandler.removeFirstMatched("pig eat me",null));
         assertEquals(null, StringHandler.removeFirstMatched(null,"add"));
         assertEquals(null, StringHandler.removeFirstMatched(null,null));
-        
-            
     }
 
 }
