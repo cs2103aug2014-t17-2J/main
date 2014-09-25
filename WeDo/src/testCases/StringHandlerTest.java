5f2703dca5a6cdd561a225c2daa4d1e228706f8b
Tien <tienlongz@gmail.com>
2014-09-25 14:57:31 +0800

    Added test cases for StringHandler functions : getFirstWord, removeFirstWord

---
+++ WeDo/src/testCases/StringHandlerTest.java
@@ -0,0 +1,29 @@
+package testCases;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import logic.StringHandler;
+
+public class StringHandlerTest {
+
+    @Test
+    public void test() {
+        assertEquals("add", StringHandler.getFirstWord("add Pizza"));
+        assertEquals(null, StringHandler.getFirstWord(""));
+        assertEquals(null, StringHandler.getFirstWord("       "));
+        assertEquals(null, StringHandler.getFirstWord(null));
+        assertEquals("greedIsGood", StringHandler.getFirstWord("greedIsGood"));
+        assertEquals("Pig", StringHandler.getFirstWord("Pig is flying !!!"));
+        
+        assertEquals("Pizza", StringHandler.removeFirstMatched("add Pizza", "add"));
+        assertEquals("pig eat me", StringHandler.removeFirstMatched("pig eat me","add"));
+        assertEquals("pig eat me", StringHandler.removeFirstMatched("pig eat me",null));
+        assertEquals(null, StringHandler.removeFirstMatched(null,"add"));
+        assertEquals(null, StringHandler.removeFirstMatched(null,null));
+        
+            
+    }
+
+}
