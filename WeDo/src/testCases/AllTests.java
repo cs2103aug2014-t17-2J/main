d8fb7289182fe42549872b24972440a1579df495
Tien <tienlongz@gmail.com>
2014-09-27 10:42:11 +0800

    Added test runner

---
+++ WeDo/src/testCases/AllTests.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package testCases;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author Kuan Tien Long
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ ObservableListTest.class, StringHandlerTest.class,
+        TaskParserTest.class })
+public class AllTests {
+
+}
