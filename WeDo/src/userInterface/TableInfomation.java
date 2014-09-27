14e8d5c7e0652c0880c3311cb8678fb94d869010
maryam <maryamisme@gmail.com>
2014-09-27 13:21:40 +0800

    added tableInformation class (for testing)

---
+++ WeDo/src/userInterface/TableInfomation.java
@@ -0,0 +1,73 @@
+package userInterface;
+
+public class TableInfomation {
+
+	protected String task, description, startDate, endDate, startTime, endTime,
+			priority;
+	
+	public void TableInformation(){
+		task = "";
+		description = "";
+		startDate = "";
+		endDate = "";
+		startTime = "";
+		endTime = "";
+		priority = "";
+	}
+	
+	public String getTask(){
+		return task;
+	}
+	
+	public void setTask(String task){
+		this.task = task;
+	}
+	
+	public String getDescription(){
+		return description;
+	}
+	
+	public void setDescription(String description){
+		this.description = description;
+	}
+	
+	public String getStartDate(){
+		return startDate;
+	}
+	
+	public void setStartDate(String startDate){
+		this.startDate = startDate;
+	}
+	
+	public String getEndDate(){
+		return endDate;
+	}
+	
+	public void setEndDate(String endDate){
+		this.endDate = endDate;
+	}
+	
+	public String getStartTime(){
+		return startTime;
+	}
+	
+	public void setStartTime(String startTime){
+		this.startTime = startTime;
+	}
+	
+	public String getEndTime(){
+		return endTime;
+	}
+	
+	public void setEndTime(String endTime){
+		this.endTime = endTime;
+	}
+	
+	public String getPriority(){
+		return priority;
+	}
+	
+	public void setPriority(String priority){
+		this.priority = priority;
+	}
+}
