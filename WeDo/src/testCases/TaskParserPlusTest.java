420a63ee759ea4f3a8329a3f4f2b3c3ba259a4ec
Tien <tienlongz@gmail.com>
2014-09-30 00:01:48 +0800

    Passed all basic parser test cases without using (hypen "-") for action words

--- WeDo/src/testCases/TaskParserPlusTest.java
+++ WeDo/src/testCases/TaskParserPlusTest.java
@@ -54,17 +54,17 @@ public class TaskParserPlusTest {
                 expectedTask,
                 taskParser
                         .buildTask("-add buy for me something date 12 sept to 18 sept priority low"));
-//
-//        expectedTask.setStartDate(LocalDate.of(2014, 9, 18));
-//        expectedTask.setStartTime(LocalTime.of(14, 0));
-//        expectedTask.setEndTime(LocalTime.of(2, 0));
-//        expectedTask.setEndDate(LocalDate.of(2014, 9, 22));
-//        expectedTask.setDescription("buy for me something");
-//        expectedTask.setPriority(Priority.PRIORITY_MEDIUM);
-//        assertEquals(
-//                expectedTask,
-//                taskParser
-//                        .buildTask("-add buy for me something -date 18 sept 2pm to 22 sept 2am -priority med"));
+
+        expectedTask.setStartDate(LocalDate.of(2014, 9, 18));
+        expectedTask.setStartTime(LocalTime.of(14, 0));
+        expectedTask.setEndTime(LocalTime.of(2, 0));
+        expectedTask.setEndDate(LocalDate.of(2014, 9, 22));
+        expectedTask.setDescription("buy for me something");
+        expectedTask.setPriority(Priority.PRIORITY_MEDIUM);
+        assertEquals(
+                expectedTask,
+                taskParser
+                        .buildTask("-add buy for me something date 18 sept 2pm to 22 sept 2am med priority"));
 
 
 
