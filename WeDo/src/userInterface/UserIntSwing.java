bdc94bc1068aa7b6be4a0d6924815fe7eefbef45
maryam <maryamisme@gmail.com>
2014-10-09 01:20:59 +0800

    fixed the textfield enter function

--- WeDo/src/userInterface/UserIntSwing.java
+++ WeDo/src/userInterface/UserIntSwing.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
@@ -17,20 +18,19 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 
 import logic.Task;
-import dataStorage.ObservableList;
-import brain.Processor;
-import ui.Keywords;
 import ui.Action;
+import ui.Keywords;
+import brain.Processor;
 
 public class UserIntSwing extends JPanel implements Observer {
 
-	ArrayList<Task> taskList;	
-	
+	ArrayList<Task> taskList;
+
 	private JFrame frame;
 	private JTextField textField;
 	private JTable table;
 	private InteractiveForm interForm;
-	
+
 	private static final String WHITESPACE_PATTERN = "\\s+";
 	private static final int MIN_TOKENS_LENGTH = 1;
 	private static final String GENERAL_GUIDE = buildGeneralGuideString();
@@ -46,6 +46,7 @@ public class UserIntSwing extends JPanel implements Observer {
 	private static final int ACTION_IDENTIFIER_INDEX = 0;
 
 	private Processor processor;
+
 	// private static final String IDENTIFIER_PLACEHOLDER = "%1$s";
 	// private static final String DATE_FORMAT = "dd/MM/yy";
 
@@ -102,35 +103,13 @@ public class UserIntSwing extends JPanel implements Observer {
 		lblDisplay.setBounds(10, 42, 157, 20);
 		frame.getContentPane().add(lblDisplay);
 
-		//ArrayList<String> taskList = new ArrayList<String>();
-
-		textField = new JTextField();
-		textField.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-
-				//String textInput = "";
-			//	textInput += textField.getText();
-				//lblDisplay.setText(textField.getText());
-				System.out.println(textField.getText()+ "hello");
-				processor.executeCommand(textField.getText());
-				//processor.executeCommand(textInput);
-				
-				//taskList.add(textInput);
-
-			//	textField.setText("");
-			}
-		});
-		textField.setBounds(10, 270, 386, 20);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-
 		JButton btnEnter = new JButton("ENTER");
 		btnEnter.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				// lblDisplay.setText(textField.getText());
-				// textArea.setText(textField.getText());
-				// textField.setText("");
+
 				processor.executeCommand(textField.getText());
+				textField.setText("");
+
 				// for (int i = 0; i < taskList.size(); i++) {
 				// textArea_1.append((String) taskList.get(i) + "\n");
 				// }
@@ -138,6 +117,20 @@ public class UserIntSwing extends JPanel implements Observer {
 		});
 		btnEnter.setBounds(406, 269, 100, 23);
 		frame.getContentPane().add(btnEnter);
+
+		textField = new JTextField();
+		// addKeyListener((KeyListener) btnEnter);
+
+		textField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				processor.executeCommand(textField.getText());
+				textField.setText("");
+			}
+		});
+		textField.setBounds(10, 270, 386, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
 
 		JLabel lblHelp = new JLabel("Label Help");
 		lblHelp.setBounds(10, 293, 496, 56);
@@ -153,6 +146,9 @@ public class UserIntSwing extends JPanel implements Observer {
 				textField.setText("-add");
 			}
 		});
+		btnAdd.setBounds(120, 11, 100, 23);
+		frame.getContentPane().add(btnAdd);
+
 		// btnAdd.addKeyListener(new KeyAdapter() {
 		// @Override
 		// public void keyPressed(KeyEvent e) {
@@ -163,21 +159,6 @@ public class UserIntSwing extends JPanel implements Observer {
 
 		// }
 		// });
-
-		textField.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-
-				lblHelp.setText(getGuideMessage("ADD"));
-
-				// textArea_1.append(taskList+"\n"); //displays the entire
-				// arrayList in [a,b,c] format
-
-				textField.setText("");
-			}
-		});
-
-		btnAdd.setBounds(120, 11, 100, 23);
-		frame.getContentPane().add(btnAdd);
 
 		JButton btnView = new JButton("F3 <View>");
 		btnView.setBounds(230, 11, 100, 23);
@@ -195,8 +176,7 @@ public class UserIntSwing extends JPanel implements Observer {
 		JButton btnDel = new JButton("F5 <Delete>");
 		btnDel.setBounds(450, 11, 100, 23);
 		frame.getContentPane().add(btnDel);
-		
-		
+
 	}
 
 	public String getGuideMessage(String commandString) {
@@ -247,8 +227,7 @@ public class UserIntSwing extends JPanel implements Observer {
 	}
 
 	@Override
-	public void update(Observable arg0, Object arg1) 
-	{
-		interForm.updateTable(taskList);		
+	public void update(Observable arg0, Object arg1) {
+		interForm.updateTable(taskList);
 	}
 }
