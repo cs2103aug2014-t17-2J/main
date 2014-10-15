32bef265f046545396cc4b09be4f77f6b151b3f2
Kuan Tien Long <tienlongz@gmail.com>
2014-10-15 23:09:03 +0800

    Added Test Case for ParserFlag

--- WeDo/src/testCases/ParserFlagTest.java
+++ WeDo/src/testCases/ParserFlagTest.java
@@ -19,7 +19,31 @@ public class ParserFlagTest {
 
     @Test
     public void test() {
+        EnumSet<ParserFlag> myFlags = EnumSet.noneOf(ParserFlag.class);
 
+        myFlags.add(ParserFlag.COMMAND_FLAG);
+        myFlags.add(ParserFlag.DESCRIPTION_FLAG);
+
+        assertTrue(ParserFlag.isParseValid(myFlags));
+
+        myFlags.remove(ParserFlag.COMMAND_FLAG);
+
+        assertFalse(ParserFlag.isParseValid(myFlags));
+
+        myFlags = EnumSet.noneOf(ParserFlag.class);
+        assertFalse(ParserFlag.isParseValid(myFlags));
+
+        myFlags.add(ParserFlag.DATE_FLAG);
+        assertFalse(ParserFlag.isParseValid(myFlags));
+
+        myFlags.add(ParserFlag.PRIORITY_FLAG);
+        assertFalse(ParserFlag.isParseValid(myFlags));
+
+        myFlags.add(ParserFlag.DESCRIPTION_FLAG);
+        assertFalse(ParserFlag.isParseValid(myFlags));
+
+        myFlags.add(ParserFlag.COMMAND_FLAG);
+        assertTrue(ParserFlag.isParseValid(myFlags));
 
     }
 
