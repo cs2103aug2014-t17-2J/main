cd45aa31e93dfa64578a0f13ac2e52419d6fcaf8
Andy Hsu <hsuperandy@hotmail.com>
2014-10-27 16:54:01 +0800

    SystemTray implemented! :D

--- WeDo/src/ui/MinimiseToTray.java
+++ WeDo/src/ui/MinimiseToTray.java
@@ -22,41 +22,44 @@ import userInterface.UserIntSwing;
  * operation
  */
 public class MinimiseToTray {
-	static TrayIcon trayIcon;
-	static SystemTray tray;
+	private static TrayIcon trayIcon;
+	private static SystemTray tray;
+	
+	private static final String FRAME_NAME = "Wedo";
+	private static final String SYSTEMTRAY_OPEN = "Open";
+	private static final String SYSTEMTRAY_EXIT = "Exit";
+	
 	public static void Minimise(WindowEvent arg){
+		
+		Image image = Toolkit.getDefaultToolkit().getImage("/ui/icon/WeDo.png");
+		PopupMenu popup = new PopupMenu();
+		trayIcon = new TrayIcon(image, FRAME_NAME, popup);
+		trayIcon.setImageAutoSize(true);
+		
 		if(SystemTray.isSupported()){
-			System.out.println("system tray supported");
+			System.out.println("SystemTray supported");
 			tray = SystemTray.getSystemTray();
-
-			Image image = Toolkit.getDefaultToolkit().getImage("/ui/icon/WeDo.png");
-			ActionListener exitListener = new ActionListener() {
+			
+			MenuItem popupItem = new MenuItem(SYSTEMTRAY_OPEN);
+			popup.add(popupItem);
+			popupItem.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
-					System.out.println("Exiting....");
-					System.exit(0);
-				}
-			};
-			PopupMenu popup = new PopupMenu();
-			MenuItem openPopup = new MenuItem("Open");
-			popup.add(openPopup);
-			MenuItem exitPopup = new MenuItem("Exit");
-			popup.add(exitPopup);
-			openPopup.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					UserIntSwing.frame.setVisible(true);
-					UserIntSwing.frame.setExtendedState(JFrame.NORMAL);
+					exitSystemTray();
 				}
 			});
-			exitPopup.addActionListener(exitListener);
-			popup.add(exitPopup);
-			
-			trayIcon = new TrayIcon(image, "WeDo", popup);
-			trayIcon.setImageAutoSize(true);
+			MenuItem popupItemExit = new MenuItem(SYSTEMTRAY_EXIT);
+			popup.add(popupItemExit);
+			popupItemExit.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					openSystemTray();
+				}
+			});
 		}
 		else{
 			System.out.println("system tray not supported");
 		}
-
+		
+		// Pass the 'iconified' parameter to minimise a frame
 		if(arg.getNewState() == Frame.ICONIFIED){
 			try{
 				tray.add(trayIcon);
@@ -66,15 +69,18 @@ public class MinimiseToTray {
 				System.out.println("unable to add to System Tray");
 			}
 		}
-		if(arg.getNewState() == Frame.MAXIMIZED_BOTH){
-			tray.remove(trayIcon);
-			UserIntSwing.frame.setVisible(true);
-			System.out.println("Tray icon removed");
-		}
-		if(arg.getNewState() == Frame.NORMAL){
-			tray.remove(trayIcon);
-			UserIntSwing.frame.setVisible(true);
-			System.out.println("Tray icon removed");
-		}
+	}
+	
+	private static void exitSystemTray(){
+		UserIntSwing.frame.setVisible(true);
+		UserIntSwing.frame.setExtendedState(JFrame.NORMAL);
+		tray.remove(trayIcon);
+		UserIntSwing.frame.setVisible(true);
+		System.out.println("Tray icon removed");
+	}
+	
+	private static void openSystemTray(){
+		System.out.println("Exiting......");
+		System.exit(0);
 	}
 }
