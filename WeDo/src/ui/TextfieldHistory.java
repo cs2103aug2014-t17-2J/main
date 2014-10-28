c6b1909d301665432830b0212160732caf7561c4
Andy Hsu <hsuperandy@hotmail.com>
2014-10-28 18:42:10 +0800

    comment and remove unnecessary imports

--- WeDo/src/ui/TextfieldHistory.java
+++ WeDo/src/ui/TextfieldHistory.java
@@ -6,45 +6,79 @@ import java.util.Stack;
 import ui.guide.FeedbackGuide;
 import userInterface.UserIntSwing;
 
+/**
+ * @author Andy Hsu Wei Qiang 
+ * This class create 2 Stacks which store the user
+ * input. 
+ * Up Arrow Listener - Display the history of what the user input
+ * Down Arrow Listener - Opposite operation of the Up Arrow Key.
+ */
 public class TextfieldHistory{
-	private static final Stack<String> textStack = new Stack<String>();
+	private static final Stack<String> userInputStack = new Stack<String>();
 	private static final Stack<String> undoStack = new Stack<String>();
-	private static String text;
+	private static String getText;
 	
+	/**
+	 * This operation checks if "undoStack" is empty. If it is not empty, pushes
+	 * all the item to "userInputStack"
+	 * After which, it gets the text from what the user type
+	 * and store it in "userInputStack" 
+	 */
 	public static void getTextfieldString(String getText){
 		
-		while(!undoStack.isEmpty()){
-			textStack.push(undoStack.pop());
+		while(!undoStack.isEmpty()) {
+			userInputStack.push(undoStack.pop());
 		}
-		textStack.push(getText);
-		for(int j=0; j<textStack.size(); j++)
-			System.out.println(textStack.get(j));
+		userInputStack.push(getText);
 	}
 	
+	/**
+	 * This operation has keylistener of up and down arrow keys
+	 * Up arrow keylistener - Pushes the text from the user to "undoStack" -Up arrow key
+	 * Down arrow keylistener - Pushes the text previously stored to "userInputStack"  
+	 */
 	public static void showTextfieldHistoryUpkey(KeyEvent arg1){
+		
 		if(arg1.getKeyCode() == KeyEvent.VK_UP){
-			if(!textStack.isEmpty()){
-				text = textStack.pop();
-				undoStack.push(text);
-				UserIntSwing.textField.setText(text);
+			if(!userInputStack.isEmpty()){
+				pushToUndoStack();
 			}
 			else{
-				UserIntSwing.lblFeedback.setText(FeedbackGuide.isEmptyHistoryString());
-				UserInterfaceMain.feedbackTimerReset();
+				showEmptyHistoryString();
 			}
 		}
 		else if(arg1.getKeyCode() == KeyEvent.VK_DOWN){
+			
 			if(!undoStack.isEmpty()){
-				text = undoStack.pop();
-				textStack.push(text);
-				UserIntSwing.textField.setText(text);
-				//System.out.println("field history counter down: " + i);
+				pushToUserInputStack();
 			}
 			else{
-				UserIntSwing.lblFeedback.setText(FeedbackGuide.isEmptyInput());
-				UserInterfaceMain.feedbackTimerReset();
+				showEmptyInputString();
 			}
 		}
 	}
 	
+	private static void pushToUndoStack(){
+		
+		getText = userInputStack.pop();
+		undoStack.push(getText);
+		UserIntSwing.textField.setText(getText);
+	}
+	
+	private static void pushToUserInputStack(){
+		
+		getText = undoStack.pop();
+		userInputStack.push(getText);
+		UserIntSwing.textField.setText(getText);
+	}
+	
+	private static void showEmptyHistoryString(){
+		UserIntSwing.lblFeedback.setText(FeedbackGuide.isEmptyHistoryString());
+		UserInterfaceMain.feedbackTimerReset();
+	}
+	
+	private static void showEmptyInputString(){
+		UserIntSwing.lblFeedback.setText(FeedbackGuide.isEmptyInput());
+		UserInterfaceMain.feedbackTimerReset();
+	}
 }
