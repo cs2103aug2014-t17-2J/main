c7d78d8d64bc6a45621bd4012034319a27afd7d5
Andy Hsu <hsuperandy@hotmail.com>
2014-11-08 12:53:02 +0800

    change file link

--- WeDo/src/ui/AboutWeDo.java
+++ WeDo/src/ui/AboutWeDo.java
@@ -1,7 +1,5 @@
 package ui;
 
-import java.awt.EventQueue;
-
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
@@ -94,16 +92,14 @@ public class AboutWeDo {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					AboutWeDo window = new AboutWeDo();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+            	AboutWeDo window = new AboutWeDo();
+				window.frame.setVisible(true);
+            }
+        });
+    }
 }
